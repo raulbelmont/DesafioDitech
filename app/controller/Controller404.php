de27bfb39232c210b8e22d069cb29ccf0d30eacf
Raul Belmonte <raulbelomont@gmail.com>
2019-09-24 19:14:00 -0300

    Atualizando Controller404

--- app/controller/Controller404.php
+++ app/controller/Controller404.php
@@ -9,6 +9,6 @@ class Controller404
 		echo "<h1>Erro 404</h1>";
 		echo "<br/>";
 		echo "<h2>Página não encontrada</h2>";
-		echo "<a href={DIRPAGE}>Voltar pro início</a>";
+		echo "<a href=".DIRPAGE.">Voltar pro início</a>";
 	}
 }
