77be75dcc66891b70c89cbcad32b864dbedaffaa
Raul Belmonte <raulbelomont@gmail.com>
2019-09-25 12:48:08 -0300

    Criando view para cadastro de usuários

---
+++ app/view/cadastro/Header.php
@@ -0,0 +1,10 @@
+<div class="row">
+  <ul class="nav bg-dark text-white col-12 justify-content-center">
+    <li class="nav-item">
+      <a class="nav-link" href="<?php echo DIRPAGE; ?>">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="<?php echo DIRPAGE.'cadastro'; ?>">Cadastre-se</a>
+    </li>
+  </ul>
+</div>
---
+++ app/view/cadastro/Main.php
@@ -0,0 +1,12 @@
+<div class="row my-3 justify-content-center">
+  <form id="formCadastro" name="formCadastro" action="<?php echo DIRPAGE.'cadastro/register' ?>" method="post" accept-charset="utf-8">
+
+    <div class="form-row">
+      <div class="form-group col-12">
+        <label for="nome">Nome</label>
+        <input id="nome" class="input-group" type="text" name="nome" placeholder="Informe seu nome"/>
+      </div>
+    </div>
+
+  </form>
+</div>
