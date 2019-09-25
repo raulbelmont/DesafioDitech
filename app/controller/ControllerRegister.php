049fdc1b821fa4ccb594cd50343019e4a4b78e00
Raul Belmonte <raulbelomont@gmail.com>
2019-09-25 14:43:29 -0300

    Cadastro de usuários finalizado

--- app/controller/ControllerRegister.php
+++ app/controller/ControllerRegister.php
@@ -3,9 +3,12 @@ namespace App\controller;
 
 use Src\classes\ClassRender;
 use Src\interfaces\InterfaceView;
+use App\model\ClassUser;
 
 class ControllerRegister extends ClassRender implements InterfaceView
 {
+	private $name;
+	private $password;
 
 	public function __construct()
 	{
@@ -16,4 +19,69 @@ class ControllerRegister extends ClassRender implements InterfaceView
 		$this->renderLayout();
 	}
 
+	#pega parâmetros do formulário para cadastro
+	public function getParameters()
+	{
+		if (isset($_POST['name']) && isset($_POST['password'])) {
+			$this->name = $_POST['name'];
+			$this->password = $this->encryptPassword($_POST['password']);
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	#criptografa senha
+	public function encryptPassword($password)
+	{
+		$encryptedPassword = md5($password);
+		return $encryptedPassword;
+	}
+
+	#realiza registro do usuário
+	public function register()
+	{
+		#setando parametros para insercao
+		if ($this->getParameters()) {
+			$user = new ClassUser();
+			$user->setName($this->name);
+			$user->setPassword($this->password);
+			#verificando se o usuário já existe
+			if ($user->userExists()) {
+				echo "<script type='text/javascript'>alert('Esse usuário já existe!')</script>";
+			#inserindo usuário
+			}elseif ($user->insert()) {
+				echo "<script type='text/javascript'>alert('Cadastrado com sucesso!')</script>";
+			}else{
+				echo "<script type='text/javascript'>alert('Erro ao inserir')</script>";
+			}
+		}else{
+			echo "<script type='text/javascript'>alert('Informe nome e senha!')</script>";
+		}
+	}
+
+	#getters and setters
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
 }
