aed6ed127937c59243d24ae1737926215a40746a
Raul Belmonte <raulbelomont@gmail.com>
2019-09-24 21:10:29 -0300

    Definindo Layout padrão

--- app/view/Layout.php
+++ app/view/Layout.php
@@ -4,26 +4,27 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="author" content="Raul Belmonte">
-  <meta name="description" content="">
-  <meta name="keywords" content="">
+  <meta name="description" content="<?php echo $this->getDescription(); ?>">
+  <meta name="keywords" content="<?php echo $this->getKeywords(); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Título</title>
+  <title><?php echo $this->getTitle(); ?></title>
   <link rel="stylesheet" href="<?php echo DIRCSS.'Style.css'; ?>">
   <link rel="stylesheet" href="<?php echo DIRCSS.'bootstrap/bootstrap.css'; ?>">
   <link rel="stylesheet" href="<?php echo DIRCSS.'Style.css'; ?>">
+  <?php $this->addHead(); ?>
 </head>
 <body class="container-fluid">
 
   <header class="row">
-
+    <?php $this->addHeader(); ?>
   </header>
 
   <main class="row">
-
+    <?php $this->addMain(); ?>
   </main>
 
   <footer class="Footer">
-
+    <?php $this->addFooter(); ?>
   </footer>
 
 <script src="<?php echo DIRJS.'jquery/jquery.js'; ?>"></script>
--- app/view/home/Header.php
+++ app/view/home/Header.php
@@ -1,13 +1,11 @@
-<div class="row">
-  <ul class="nav justify-content-center bg-dark text-white col-12">
-    <li class="nav-item">
-      <a class="nav-link" href="<?php echo DIRPAGE; ?>">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="<?php echo DIRPAGE.'entrar'; ?>">Entrar</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="<?php echo DIRPAGE.'cadastro'; ?>">Cadastre-se</a>
-    </li>
-  </ul>
-</div>
+<ul class="nav justify-content-center bg-dark text-white col-12">
+  <li class="nav-item">
+    <a class="nav-link" href="<?php echo DIRPAGE; ?>">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="<?php echo DIRPAGE.'entrar'; ?>">Entrar</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="<?php echo DIRPAGE.'cadastro'; ?>">Cadastre-se</a>
+  </li>
+</ul>
