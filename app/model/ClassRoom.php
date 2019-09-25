72564039974bfe470a12cb13bf550109cf790400
Raul Belmonte <raulbelomont@gmail.com>
2019-09-25 00:33:00 -0300

    Criando classe de salas

---
+++ app/model/ClassRoom.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\model;
+
+use App\model\ClassCRUD;
+
+class User extends ClassCRUD
+{
+
+	private $id;
+	private $roomNumber;
+	protected $table = 'user';
+
+	public function insert()
+	{
+		$sql = "INSERT INTO $this->table (roomNumber) VALUES (:roomNumber)";
+		$stmt = Conecta::prepare($sql);
+		$stmt->bindParam(':roomNumber', $this->roomNumber, PDO::PARAM_INT);
+		return $stmt->execute();
+	}
+
+	public function update()
+	{
+
+	}
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getRoomNumber()
+    {
+        return $this->roomNumber;
+    }
+
+    public function setRoomNumber($roomNumber)
+    {
+        $this->roomNumber = $roomNumber;
+
+        return $this;
+    }
+
+}
