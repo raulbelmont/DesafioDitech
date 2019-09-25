6fb6d0a4d16472e3e605ddcc5a6505beec4ff6b8
Raul Belmonte <raulbelomont@gmail.com>
2019-09-25 13:04:43 -0300

    Finalizando formulário de cadastro

--- app/view/cadastro/Main.php
+++ app/view/cadastro/Main.php
@@ -1,12 +1,27 @@
 <div class="row my-3 justify-content-center">
+
+  <h2 class="col-12 text-center font-weight-bold">Cadastro</h2>
+
   <form id="formCadastro" name="formCadastro" action="<?php echo DIRPAGE.'cadastro/register' ?>" method="post" accept-charset="utf-8">
 
     <div class="form-row">
+      <!-- Nome -->
       <div class="form-group col-12">
         <label for="nome">Nome</label>
-        <input id="nome" class="input-group" type="text" name="nome" placeholder="Informe seu nome"/>
+        <input id="nome" class="input-group" type="text" name="nome" placeholder="Informe seu nome" required/>
       </div>
+
+      <!-- Senha -->
+      <div class="form-group col-12">
+        <label for="senha">Senha</label>
+        <input id="senha" class="input-group" type="password" name="senha" placeholder="Digite uma senha" required/>
+      </div>
+
+       <button type="submit" class="btn btn-primary">Cadastrar</button>
+
     </div>
 
   </form>
+
+  <p class="d-block col-12 text-center mt-3"><a href="<?php echo DIRPAGE.'home' ?>">Já sou cadastrado.</a></span>
 </div>
